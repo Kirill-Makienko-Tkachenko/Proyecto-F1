c030659ca7f02787bf75c3abb82924dbfd4ec3ab
GOM <elchiqvo@gmail.com>
2022-11-25 15:16:46 -0600

    Casi excel completo

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('Aqui van a escribirse cosas bien chingonas')
+# Simulacion Formula 1
+# 25/11/2022
+# Kirill Makienko Tkachenko
+# Dulce Nahomi Bucio Rivas
+# Desireé Espinosa Contreras
+# Samuel Eric Miranda Álvarez
+# Oscar Omar Cepeda Vázquez 
+
+import math
+import csv
+
+
+# como nadien de ustedes estaban, dulce y kirill tomamos la desicion ejecutiva de trabajar con python default, sin pandas
+
+# Importar .csv de excel
+with open(r"coordenadas_autodromo.csv",'r') as file:
+        reader= csv.reader(file)
+        x = 0
+        y = 0
+        for row in reader:
+            
+            #row[0]     row[1]      row[2]              row[3]              row[4]
+    #   Coordenada X  Coordenada Y  Tipo de segmento    Velocidad maxima    Helper_id
+            #print(row[0], "\t" , row[1], "\t" , row[2], "\t" , row[3], "\t" , row[4])
+            distanciaX = x - float(row[0])
+            distanciaY = y - float(row[1])
+            x = row[0]
+            y = row[1]
+            print("Distancia en X de estos 2 puntos: ", distanciaX, "Distancia en Y de estos 2 puntos: ", distanciaY)
