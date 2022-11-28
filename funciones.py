671f5ce3554195398009296727deb33d85f9e48b
GOM <elchiqvo@gmail.com>
2022-11-27 18:48:58 -0600

    Deteccion segmentos

--- funciones.py
+++ funciones.py
@@ -1,3 +1,17 @@
+# Simulacion Formula 1
+# 25/11/2022
+# Kirill Makienko Tkachenko
+# Agreguen sus nombres si abren este archivo
+
+
+
+import math
+
+
 def aceleracion(v1,v2,v3,v4,v5):
     x = (v1+v2+v3+v4+v5)/5
     return x
+
+def distanciaSiguienteSegmento(x0,x1,y0,y1):
+    resultado = math.sqrt(((x1-x0)**2)+((y1-y0)**2))
+    return resultado
--- main.py
+++ main.py
@@ -27,8 +27,13 @@ arrX = []
 arrY = []
 tipoCurva = []
 velMax = []
-arrAyuda = [0,0,0,0,0] #Añadir 5 ceros para poder hacer i+1,i+2,i+3,i+4
-arrCur = [2,2,2,2,2] #Añadir 5 Dos para poder hacer i+1,i+2,i+3,i+4
+
+#velMaxC = int(input()) # Velocidad Maxima coche
+#pesoC = int(input()) # Peso del coche
+#aceleracionC = int(input()) # Aceleracion del coche
+#frenadoC = int(input()) # Capacidad de frenado del coche
+#friccionC = int(input()) # Coeficiente de friccion de las llantas con el piso
+#resistenciaC = int(input()) # Resistencia del aire con respecto al coche
 
 
 # Importar .csv de excel y escribir sus datos en un array de python
@@ -37,31 +42,58 @@ with open(r"coordenadas_autodromo.csv",'r') as file:
         for row in reader:
             arrX.append(float(row[0]))
             arrY.append(float(row[1]))
-            tipoCurva.append(float(row[2]))
+            tipoCurva.append(int(row[2]))
             velMax.append(row[3])
             #row[0]     row[1]      row[2]              row[3]              row[4]
     #   Coordenada X  Coordenada Y  Tipo de segmento    Velocidad maxima    Helper_id
     #                         Tipo de segmento: 0 - recta, 1 - curva
             #print(row[0], "\t" , row[1], "\t" , row[2], "\t" , row[3], "\t" , row[4])
+file.close()
+
+i = 0
+while i < 5: # Este Append es para decirle al programa que si puede ir a "i" + de 332 para saber la distancia que falta para el siguiente tipo de segmento
+        arrX.append(0)
+        arrY.append(0)
+        velMax.append(0)
+        tipoCurva.append(0)
+        i += 1
 
 
-arrX.append(arrAyuda)
-arrY.append(arrAyuda)
-velMax.append(arrAyuda)
-tipoCurva.append(arrCur)
+
+
+
+
+# Por hacer en esta seccion
+# hacer que sirva con caso 0 0 1 0, tal vez meter esta cosa en algun array o algos
 
 
 i = 0
-
+p = 3
+vel = 0 # Velocidad inicial del coche = 0, parte del reposo
 while i < 332:
-        print(arrX[i],arrX[i+1],arrX[i+2],arrX[i+3],arrX[i+4])
-        v1 = arrX[i]
-        v2 = arrX[i+1]
-        v3 = arrX[i+2]
-        v4=  arrX[i+3]
-        v5=  arrX[i+4]
-        vf = aceleracion(v1,v2,v3,v4,v5)
-        print(vf)
+# Vamos a hacer logica de segmentos rectos
+        if int(tipoCurva[i]) == 0:
+                
+                input()
+                if int(tipoCurva[i+p]) or int(tipoCurva[i+p-1]) == 1:
+                        disTot = distanciaSiguienteSegmento(arrX[i],arrX[i+p],arrY[i],arrY[i+p])
+                        print("Distancia total hasta siguiente segmento curvo: " , disTot)
+#                        distancia disponible > distancia para frenar:
+                        p -= 1
+                        
+                
+        if int(tipoCurva[i]) == 1:
+                print(tipoCurva[i], tipoCurva[i+1],tipoCurva[i+2],tipoCurva[i+p])
+                input()
+                if int(tipoCurva[i+p]) == 0:
+                        disTot = distanciaSiguienteSegmento(arrX[i],arrX[i+p],arrY[i],arrY[i+p])
+                        print("Distancia total hasta siguiente segmento recto: " , disTot)
+#                        distancia disponible > distancia para frenar:
+                        p -= 1
+                
+                #acelerar
+        if p == 0:
+                p = 3
         i += 1
 
 
